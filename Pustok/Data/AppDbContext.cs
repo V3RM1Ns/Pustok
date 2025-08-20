cc32ec3557a96da92d29cc0d8df24871971b3c35
V3RM1Ns <rovsanoglu@gmail.com>
2025-08-20 17:32:03 +0400

    books

---
+++ Pustok/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Pustok.Models;
+
+namespace Pustok.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Slider> Sliders { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<BookImg> BookImages { get; set; }
+    
+}
