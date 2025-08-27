b77c5594e125b74bfbbf5284900b263da0cd9cc2
V3RM1Ns <rovsanoglu@gmail.com>
2025-08-27 14:41:43 +0400

    asdd

--- Pustok/obj/Debug/net9.0/Pustok.AssemblyInfo.cs
+++ Pustok/obj/Debug/net9.0/Pustok.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pustok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68ecf169f3629b527f404e6ea8b98b01f016c975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c9485ed9ce4077381a6d88abc47d64b0a79851")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pustok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pustok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
